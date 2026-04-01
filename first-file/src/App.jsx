efa17b4d4eb1ea952c3e68784234559cb8cd21e4
Abhay Gupta <abhaygupta83977@gmail.com>
2026-04-02 01:59:19 +0530

    first-file

---
+++ first-file/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to react jurney</h1>
+      <h2>Best of luck</h2>
+    </div>
+  )
+} 
+
+export default App
