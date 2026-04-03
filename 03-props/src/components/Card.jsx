f883a61e9d4f216e85efcfd1437e453583b7fd96
Abhay Gupta <abhaygupta83977@gmail.com>
2026-04-03 10:51:39 +0530

    03-props

---
+++ 03-props/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = (props) => {
+  
+  return (
+     
+        <div className="card">
+        <img src={props.img} alt="" />
+        <h1>{props.name} {props.age}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, id.</p>
+        <button>View Profile</button>
+       </div>
+       
+        
+      
+      
+   
+  )
+}
+
+export default Card
