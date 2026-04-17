fffb0bd9984a56451ee55d6bb4f42981301a75b6
Abhay Gupta <abhaygupta83977@gmail.com>
2026-04-17 12:38:53 +0530

    only information

---
+++ 14-local-storage/src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const App = () => {
+
+
+  
+//  esme  ham keval local storage k baare me jaane ge ko lokal storage kya hota hay 
+// kaise kam karta hay 
+// esme ham jaanege  local storage k method k baare me or next folder  me unki practice karege 
+// 
+
+// local storage :
+//  local storage save data  permanentaly  in browser 
+// ecven aap  browser bard kar do tab band kar do ya leptop phone  jo bhi hay use band kar do tab bhi local storage me data seve rahega 
+
+
+// ese tabhi cleare kar sakte hay jab ham esk method k baare me jaane ge or samjhe ge 
+
+
+
+
+// importent local storage method :
+
+// 1: setItems 
+// esse ham data ko set kaar sakte hay 
+// details se   jaanna hay to next folder me dekhe 
+
+// 2: getItems 
+// esse hm data ko dekh sakte hay
+// console me bhi print karva k check kar sakte hay 
+
+
+
+// 3: remove items
+// esse  ham data ko jo bhi hamne save kiaa hay  ya phle se save hay use ham delete kar sakte hay
+// kaise usko jaane k lia opent next folder
+
+// 4: clear
+// esse ham data ko delete kaar sakte hay permanently
+
+
+
+// one more importent thing
+// local storage me ham array or obj ko bhi save kar sakte hay 
+// ab enko kaise sabe karte hay  esko jaane k lia hme esk next folder me jaa k hi pta chalega 
+
+// 
+
+// so go to the next folder and explore now
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
