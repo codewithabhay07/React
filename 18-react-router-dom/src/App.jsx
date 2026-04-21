f1841ab8d22044b4f71ac11b96bbb7b57a15f6d3
Abhay Gupta <abhaygupta83977@gmail.com>
2026-04-21 14:20:12 +0530

    basic routing

---
+++ 18-react-router-dom/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/contact'
+import Product from './pages/product'
+import Navbar from './component/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      
+      <Routes>
+        <Route path='/' element = {<Home />}/>
+        <Route path='/about' element =  {<About />}/>
+        <Route path='/contact' element =  {<Contact />}/>
+        <Route path='/product' element =  {<Product />}/>
+      </Routes>   
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 18-react-router-dom/src/component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='nav'>
+        <h3>Sheryians Coding School</h3>
+        <div>
+          <Link to="/"> Home</Link>
+          <Link to="/About"> About</Link>
+          <Link to="/Contact"> Contact</Link>
+          <Link to="/Product"> Product</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
